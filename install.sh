82364b336ee39b69a99af995c1a08a1607bf016d
Joshua Clayton <joshua.clayton@lvt.com>
2025-03-10 15:41:52 -0600

    feat(install): improve platform compatibility
    First, enable action to work correctly on all supported platforms.
    Check the OS and Architecture of the runner, download the correct version
    if available, and bail out with an appropriate error if not supported.
    
    Second, behave like a github setup- tool.
    This means download to a versioned subdirectory in the GHA tool cache, and
    don't download again if the binary is already there.
    (also delete the tarball as part of the install).
    Allow the user to optionally select the cog version

--- install.sh
+++ install.sh
@@ -1,12 +1,36 @@
 #!/bin/sh
+BIN_DIR="$RUNNER_TOOL_CACHE/cocogitto/$COG_VERSION"
 
-CUR_DIR=$(pwd)
-VERSION=6.2.0
-TAR="cocogitto-$VERSION-x86_64-unknown-linux-musl.tar.gz"
-BIN_DIR="$HOME/.local/bin"
+if ! [ -f $BIN_DIR/cog ]; then
+    case ${RUNNER_ARCH}_${RUNNER_OS} in
+    ARM_Linux)
+        COG_VARIANT="armv7-unknown-linux-musleabihf"
+        ;;
+    ARM64_Linux)
+        COG_VARIANT="aarch64-unknown-linux-gnu"
+        ;;
+    X64_macOS)
+        COG_VARIANT="x86_64-apple-darwin"
+        ;;
+    X64_Linux)
+        COG_VARIANT="x86_64-unknown-linux-musl"
+        ;;
+    X64_Windows)
+        COG_VARIANT="x86_64-pc-windows-msvc"
+        ;;
+    *)
+        echo "::error:: cog is not supported on ${RUNNER_ARCH} and ${RUNNER_OS}"
+        echo See https://github.com/cocogitto/cocogitto/releases
+        exit 1
+        ;;
+    esac
 
-mkdir -p "$BIN_DIR"
-cd "$BIN_DIR" || exit
-curl -OL https://github.com/cocogitto/cocogitto/releases/download/"$VERSION"/"$TAR"
-tar --strip-components=1 -xzf $TAR x86_64-unknown-linux-musl/cog
-cd "$CUR_DIR" || exit
+    TAR="cocogitto-${COG_VERSION}-${COG_VARIANT}.tar.gz"
+    mkdir -p "$BIN_DIR"
+    cd "$BIN_DIR" || exit
+    curl -OL https://github.com/cocogitto/cocogitto/releases/download/"$COG_VERSION"/"$TAR"
+    tar --strip-components=1 -xzf $TAR
+    rm $TAR
+fi
+
+echo "$BIN_DIR" >> "$GITHUB_PATH"
